b31181350b8adfe553cdb95b4882bf8b51f1d7db
NingNingY <15829479951@163.com>
2025-12-26 16:49:19 +0800

    init

--- src/components/Nyn/Git/index.vue
+++ src/components/Nyn/Git/index.vue
@@ -1,11 +1,9 @@
 <template>
   <div>
-    <svg-icon icon-class="github" @click="goto" />
   </div>
 </template>
 
 <script setup>
-const url = ref('https://gitee.com/y_project/nyn-Vue')
 
 function goto() {
   window.open(url.value)
